12998c171ca1ca33eebf093cd5b4cf4f9e60b2ef
Daniël <daniellinye@hotmail.com>
2017-01-19 12:13:32 +0100

    made background import

--- Game files/__init__.py
+++ Game files/__init__.py
@@ -20,6 +20,7 @@ class iV:
         self.green = (0, 255, 0)
         self.red = (255, 0, 0)
         self.white = (255,255,255)
+        self.background = Background('./assets/background.PNG', [0,0])
 
         self.score = 0
         self.width = 800
@@ -28,9 +29,15 @@ class iV:
 
         self.screen = pygame.display.set_mode(self.size)
 
+class Background(pygame.sprite.Sprite):
+    def __init__(self, image, location):
+        pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+        self.image = pygame.image.load(image)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
 
 
-
+init = iV()
 
 
 
