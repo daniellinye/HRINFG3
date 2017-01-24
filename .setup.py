658df1fa0987787ec53c3ec2efa122c496afa037
Oscar druivenetak <oscar_rank_46@hotmail.com>
2017-01-24 17:01:44 +0100

    added setup script

--- .setup.py
+++ .setup.py
@@ -2,6 +2,9 @@
 # settings.py
 from os.path import join, dirname
 from dotenv import load_dotenv
-
+from Game_Files.database.db import Database
 dotenv_path = join(dirname(__file__), '.env')
 load_dotenv(dotenv_path)
+
+# run migrations and seed
+Database().migrateAndSeed()
