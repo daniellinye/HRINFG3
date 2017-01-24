7b07623806b1c5da73c103ee5970ce1310f8fe13
Monne <monne_weeda@hotmail.com>
2017-01-24 15:09:18 +0100

    Dice added

---
+++ Game_Files/Dice.py
@@ -0,0 +1,84 @@
+import pygame
+from random import randint
+
+#inspection cannot resolve names
+from __init__ import *
+from libdef import *
+
+#starting inits
+game = IV()
+
+
+class DrawDiceScreen:
+    def __init__(self):
+        self.drawing = True
+
+    def drawScreen(self):
+        # background
+        game.screen.fill((255, 255, 255))
+
+        # roll button
+        self.roll = DrawButton(game.screen, game.green, game.white, "Roll Dice", 500, 125, (game.width * 0.5)-250,
+                               (game.height * 0.5)-62.5)
+
+    def logicDrawScreen(self):
+        self.roll.collision(game.red)
+
+        # flip updated screen
+        if self.roll.collision():
+            self.number = randint(1,6)
+            return 101
+
+    def drawScreen1(self):
+        # background
+        game.screen.fill((255, 255, 255))
+        # dice image
+        img = pygame.image.load('./assets/white_dice/{}.png'.format(self.number))
+        game.screen.blit(img, [game.width*0.5-60, game.height*0.2])
+        # text beneath image
+        self.text = DrawText(game.screen, "You rolled a {}!".format(self.number), game.red, game.width-512, game.height*0.4)
+        # continue button
+        self.continu = DrawButton(game.screen, game.green, game.white, "Continue", 200, 50, (game.width * 0.5)-100,(game.height * 0.5)-25)
+
+    def logicDrawScreen1(self):
+        self.continu.collision(game.red)
+
+        # flip updated screen
+        if self.continu.collision():
+            return 102
+
+
+
+
+# force quit event
+def process_events():
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            return False
+    return True
+
+class RollDice:
+    state = 100
+    running = True
+    toDraw = DrawDiceScreen()
+    while process_events() and running:
+        # FPS
+        game.clock.tick(game.fps)
+        # -----------------------------
+        if state == 100:
+            # keeps drawing screen
+            toDraw.drawScreen()
+            # logic0
+            if toDraw.logicDrawScreen() == 101:
+                state = 101
+        # -----------------------------
+        elif state == 101:
+            toDraw.drawScreen1()
+            if toDraw.logicDrawScreen1() == 102:
+                state = 102
+        elif state == 102:
+            running = False
+        pygame.display.flip()
+
+
+rollDice = RollDice()
