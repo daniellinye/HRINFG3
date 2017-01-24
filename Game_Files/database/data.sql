ec6e0fc4d374381bd64752743735fc4ad202ef4a
Oscar druivenetak <oscar_rank_46@hotmail.com>
2017-01-24 17:12:26 +0100

    added category table

--- Game_Files/database/data.sql
+++ Game_Files/database/data.sql
@@ -4,9 +4,13 @@ CREATE TABLE IF NOT EXISTS question (
   category_id INT NOT NULL
 );
 
-
 CREATE TABLE IF NOT EXISTS answer (
   id serial NOT NULL primary key,
   name VARCHAR(255) NOT NULL,
   question_id INT NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS category (
+  id serial NOT NULL primary key,
+  name VARCHAR(255) NOT NULL
 );
