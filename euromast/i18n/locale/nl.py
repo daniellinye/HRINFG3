13b0c0c0aabb3eba82b9e6d6f6bc8fc23f21cad6
Jasper1533 <jasper1533@gmail.com>
2017-02-02 17:04:04 +0100

    Fixed instructions screen

--- euromast/i18n/locale/nl.py
+++ euromast/i18n/locale/nl.py
@@ -1,20 +1,20 @@
 lang = {
     "start": 'Start',
-    "resume": "hervat",
-    "go to menu": "naar menu",
+    "resume": "Hervat",
+    "go to menu": "Naar menu",
     "settings": 'Instellingen',
     "instructions": 'Instructies',
     "highscores": 'Highscore',
     "exit": "Verlaten",
     "language": "Taal",
-    "continue": "ga door",
-    "go back": "ga terug",
+    "continue": "Ga door",
+    "go back": "Ga terug",
     "rules": "Regels",
     "select amount of players": "Selecteer het aantal spelers",
     "2 players": "2 spelers",
     "3 players": "3 spelers",
     "4 players": "4 spelers",
-    "guide": "- Er zijn 2-4 spelers (leeftijd 14+)\n \n " \
+    "guide1": "- Er zijn 2-4 spelers (leeftijd 14+)\n \n " \
                       "- Er is een speelbord (De Euromast)\n Zie de instructies om te weten hoe het speelbord in elkaar moet worden gezet.\n \n " \
                       "- Er is een zandloper.\n Voor het meten van de 50 seconden om een vraag te beantwoorden (de tijd start wanneer een niet-spelende speler de vraag hardop voorleest).\n \n " \
                       "- Er is één dobbelsteen. De dobbelsteen bevat de nummers 1 t/m 6. Elk nummer heeft zijn eigen soort vraag.\n " \
@@ -30,8 +30,8 @@ lang = {
                       "Wanneer twee of meer spelers het hoogste cijfer gooien, zullen deze spelers de dobbelsteen nogmaals gooien totdat één van die spelers het hoogste cijfer gooit.\n \n " \
                       "- De rest van de spelers kiezen een categorie van de overgebleven categorieën (er is maar 1 speler per categorie mogelijk aan het begin van het spel).\n \n " \
                       "- De speler wie het hoogste cijfer heeft gegooid, zal als eerste beginnen met zijn/haar beurt. Nadat deze speler klaar is met zijn/haar beurt, vervolgt het spel zich met de klok mee.\n " \
-                      "\n - Spelers plaatsen hun poppetjes op het houten deel van het speelbord en voor de gekozen categorie.\n Plaats het poppetje niet in het eerst gaatje.\n \n \n " \
-                      "Tijdens het spel:\n " \
+                      "\n - Spelers plaatsen hun poppetjes op het houten deel van het speelbord en voor de gekozen categorie.\n Plaats het poppetje niet in het eerst gaatje.\n \n \n ",
+    "guide2": "Tijdens het spel:\n " \
                       "- Kies een als eerst een richting (links, rechts, omhoog, omlaag). Nadat er een richting is gekozen word de dobbelsteen gegooid om te bepalen hoeveel stappen er genomen \n mogen worden.\n" \
                       " (Het is alleen toegestaan om een poppetje in horizontale of verticale richting te verplaatsen)\n " \
                       "- cijfers 1 en 2 = 1 stap in gekozen richting\n " \
@@ -61,5 +61,6 @@ lang = {
     "the playing order will be": "De speel volgorde is:",
     "sound": "Geluid",
     "music": "Muziek",
-    "effects": "Geluiden"
+    "effects": "Geluiden",
+    "next": "Volgende"
 }
